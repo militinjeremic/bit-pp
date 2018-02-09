cd9eabca6c16b024c473300cfb0bade13a51b082
BIT Student <classroom@bgit.rs>
2018-02-09 16:19:13 +0100

    finished 3

--- day10/exctersise_3.js
+++ day10/exctersise_3.js
@@ -6,14 +6,15 @@ function comb(a, b) {
     var k = 0;
     var o = 0;
     var c = [];
-    for (i = 0, j = 0, k = 0; i < a.length && j < b.length; i++ , j++ , k++) {
+    for (i = 0, j = 0, k = 0; i < a.length && j < b.length; i++ , j++) {
         c[k] = a[i];
-
-    }
-    for (j = 0; j < b.length; k++) {
+        k++;
         c[k] = b[j];
-
+k++;
     }
+
     return c;
-}
+    }
+   
+
 console.log(comb([1, 3, 5, 7, 9], [2, 4, 6, 8, 10]));
