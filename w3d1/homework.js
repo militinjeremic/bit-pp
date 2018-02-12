620d4da75e6e76eb13059e579483f5d0c1da6e6e
milutinjeremic <milutin.jeremic@gmail.com>
2018-02-12 20:28:14 +0100

    exercise 5 done

---
+++ w3d1/homework.js
@@ -0,0 +1,13 @@
+function multipTable(a){
+    var i;
+    var j;
+    sum = "";
+    for(i = 0; i < a;i++){
+        for (j=0; j < a ;j++){
+            sum = sum + " " + i * j;
+        }
+        sum = sum +"\n";
+    }
+return sum;
+}
+console.log(multipTable(13));
