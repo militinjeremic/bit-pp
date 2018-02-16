94ddc5541b49b25da11e54c9c6e75ad8c4626004
BIT Student <classroom@bgit.rs>
2018-02-16 13:08:57 +0100

    objects practical

---
+++ w3/d5/Methods.js
@@ -0,0 +1,41 @@
+function convert(x) {
+
+      return x.toString(16);
+    }
+
+  console.log(convert(12));
+//-------------------------------
+
+function reverse(x){  
+
+
+}
+
+console.log(reverse(256));
+//---------------
+
+
+var myString = 'JavaScript-function-that reverses a number';
+var splits = myString.split(' ', 2);
+
+console.log(splits);
+//-------------
+
+
+
+
+var abb = "Belgrade Institute of Technology torture people!!! xD...";
+
+var s = abb.split(' ', 4);
+console.log(s);
+
+for ( var i=0; i<s.length; i++) {
+    
+return abb2;
+}
+     
+  //abb2 = s.slice(11,-6);
+
+
+
+console.log(abb2);
