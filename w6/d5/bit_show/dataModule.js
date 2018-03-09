cf8d23c6cfae31a88c9eb344fd4a1892699a95c2
BIT Student <classroom@bgit.rs>
2018-03-09 17:44:13 +0100

    bit-show begin

---
+++ w6/d5/bit_show/dataModule.js
@@ -0,0 +1,44 @@
+var dataModule = (function () {
+    
+
+    return {
+
+    }
+
+    
+    })();
+
+    class tvShows {
+        constructor(show) {
+            var shows = [];
+            
+        }
+    }
+
+    class Show {
+        constructor(name, imageUrl) {
+            this.name = name;
+            this.imageUrl = imageUrl;
+
+        }
+        getName(){
+            return name;
+        }
+    }
+    
+    class Cast {
+        constructor() {
+
+        }
+    }
+    class Season {
+        constructor() {
+            this.date = date;
+        }
+    }
+
+
+
+
+   
+
